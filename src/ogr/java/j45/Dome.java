157daa12d5dff00906617a46b03b13fd502cdbe6
yh <1062296459@qq.com>
2017-12-23 15:05:56 +0800

    第一次提交

---
+++ src/ogr/java/j45/Dome.java
@@ -0,0 +1,12 @@
+package ogr.java.j45;
+
+public class Dome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1111111
+		// 22222222
+		System.out.println("33333333");
+	}
+
+}
